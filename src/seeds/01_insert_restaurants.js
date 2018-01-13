867fd0fbe353f576b997f8630dbb71d9033e5e6b
Joonas Tiala <joonas.tiala@gmail.com>
2018-01-13 22:09:18 +0200

    Added seeds for imports

--- src/seeds/01_insert_restaurants.js
+++ src/seeds/01_insert_restaurants.js
@@ -1,36 +1,37 @@
 /* eslint-disable import/prefer-default-export */
 /**
  * Seed restaurants table.
+ *
  * @param  {object} knex
  * @param  {object} Promise
  * @return {Promise}
  */
 export function seed(knex, Promise) {
-  // Deletes all existing entries
   return knex('restaurants')
     .del()
-    .then(() =>
-      // Inserts seed entries
-      Promise.all([
-        knex('restaurants').insert([
-          {
-            name: 'Smarthouse',
-            chain: 'Amica',
-            url: 'https://www.amica.fi/ravintolat/ravintolat-kaupungeittain/oulu/smarthouse/',
-            updated_at: new Date(),
-          },
-          {
-            name: 'Garden',
-            chain: 'Amica',
-            url: 'https://www.amica.fi/ravintolat/ravintolat-kaupungeittain/oulu/teknologiantie-1/',
-            updated_at: new Date(),
-          },
-          {
-            name: 'VTT Oulu',
-            chain: 'Amica',
-            url: 'https://www.amica.fi/ravintolat/ravintolat-kaupungeittain/oulu/vtt-oulu/',
-            updated_at: new Date(),
-          },
-        ]),
-      ]));
+    .then(() => Promise.all([
+      knex('restaurants').insert([
+        {
+          id: 1,
+          name: 'Smarthouse',
+          chain: 'Amica',
+          url: 'https://www.amica.fi/ravintolat/ravintolat-kaupungeittain/oulu/smarthouse/',
+          updated_at: new Date(),
+        },
+        {
+          id: 2,
+          name: 'Garden',
+          chain: 'Amica',
+          url: 'https://www.amica.fi/ravintolat/ravintolat-kaupungeittain/oulu/teknologiantie-1/',
+          updated_at: new Date(),
+        },
+        {
+          id: 3,
+          name: 'VTT Oulu',
+          chain: 'Amica',
+          url: 'https://www.amica.fi/ravintolat/ravintolat-kaupungeittain/oulu/vtt-oulu/',
+          updated_at: new Date(),
+        },
+      ]),
+    ]));
 }
---
+++ src/seeds/02_insert_imports.js
@@ -0,0 +1,37 @@
+/* eslint-disable import/prefer-default-export */
+/**
+ * Seed imports table.
+ *
+ * @param  {object} knex
+ * @param  {object} Promise
+ * @return {Promise}
+ */
+export function seed(knex, Promise) {
+  return knex('imports')
+    .del()
+    .then(() => Promise.all([
+      knex('imports').insert([
+        {
+          updated_at: new Date(),
+          importer: 'amicaImporter',
+          schedule: '* * * * *',
+          identifier: '3498',
+          restaurant_id: 1,
+        },
+        {
+          updated_at: new Date(),
+          importer: 'amicaImporter',
+          schedule: '* * * * *',
+          identifier: '3497',
+          restaurant_id: 2,
+        },
+        {
+          updated_at: new Date(),
+          importer: 'amicaImporter',
+          schedule: '* * * * *',
+          identifier: '3587',
+          restaurant_id: 3,
+        },
+      ]),
+    ]));
+}
