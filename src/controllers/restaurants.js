34819a1ac807bceef8cd8d922af11147b6e900f2
Joonas Tiala <joonas.tiala@gmail.com>
2018-01-14 21:11:51 +0200

    Remove restaurant write endpoints (closes #7)

--- src/controllers/restaurants.js
+++ src/controllers/restaurants.js
@@ -1,7 +1,5 @@
 import { Router } from 'express';
-import HttpStatus from 'http-status-codes';
 import * as restaurantService from '../services/restaurantService';
-import { findRestaurant, restaurantValidator } from '../validators/restaurantValidator';
 
 const router = Router();
 
@@ -22,36 +20,6 @@ router.get('/:id', (req, res, next) => {
   restaurantService
     .getRestaurant(req.params.id)
     .then(data => res.json({ data }))
-    .catch(err => next(err));
-});
-
-/**
- * POST /v1/restaurants
- */
-router.post('/', restaurantValidator, (req, res, next) => {
-  restaurantService
-    .createRestaurant(req.body)
-    .then(data => res.status(HttpStatus.CREATED).json({ data }))
-    .catch(err => next(err));
-});
-
-/**
- * PUT /v1/restaurants/:id
- */
-router.put('/:id', findRestaurant, restaurantValidator, (req, res, next) => {
-  restaurantService
-    .updateRestaurant(req.params.id, req.body)
-    .then(data => res.json({ data }))
-    .catch(err => next(err));
-});
-
-/**
- * DELETE /v1/restaurants/:id
- */
-router.delete('/:id', findRestaurant, (req, res, next) => {
-  restaurantService
-    .deleteRestaurant(req.params.id)
-    .then(data => res.status(HttpStatus.NO_CONTENT).json({ data }))
     .catch(err => next(err));
 });
 
